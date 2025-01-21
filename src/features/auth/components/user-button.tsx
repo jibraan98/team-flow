2cc97936278759d60a79a3d4a8277bda9e2a57b7
jibraan98 <jibraan2010@hotmail.com>
2025-01-21 15:23:45 +0400

    channel and members

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -25,9 +25,9 @@ export const UserButton = () => {
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
-                <Avatar className="size-10 hover:opacity-75 transition">
-                   <AvatarImage alt={name} src={image}/>
-                   <AvatarFallback className="bg-sky-500 text-white">
+                <Avatar className="rounded-md size-10 hover:opacity-75 transition">
+                   <AvatarImage alt={name} src={image} className="rounded-md"/>
+                   <AvatarFallback className="bg-sky-500 text-white rounded-md">
                         {avatarFallback}
                     </AvatarFallback> 
                 </Avatar>
