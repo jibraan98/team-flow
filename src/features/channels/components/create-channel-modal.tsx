e440299e6160ccbb9c5b8cb0eef9ef452c0ccd9d
jibraan98 <jibraan2010@hotmail.com>
2025-01-22 14:25:05 +0400

    added channel page

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -5,9 +5,12 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { useCreateChannel } from "../api/use-create-channel";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export const CreateChannelModal = () => {
     const [open, setOpen] = useCreateChannelModal();
+    const router = useRouter();
 
     const workspaceId = useWorkspaceId();
 
@@ -31,9 +34,14 @@ export const CreateChannelModal = () => {
             { name, workspaceId },
             {
                 onSuccess: (id) => {
+                    toast.success("Channel created")
+                    router.push(`/workspace/${workspaceId}/channel/${id}`);
                     handleClose();
+                },
+                onError: () => {
+                    toast.error("Failed to create channel");
                 }
-            }
+            },
         )
     }
 
